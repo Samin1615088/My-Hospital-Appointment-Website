27d9030a6e4854341ee0828a51754fd17d216c44
Samin1615088 <samin.shafkat@gmail.com>
2021-04-11 21:22:28 +0600

    first commit

---
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HeaderMain = () => {
+    return (
+        <main className="row">
+            
+            
+        </main>
+    );
+};
+
+export default HeaderMain;
