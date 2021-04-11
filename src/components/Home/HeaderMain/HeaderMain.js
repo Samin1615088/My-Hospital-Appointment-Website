ca8c30c3f3d938be849c58ec83624e5992510da5
Samin1615088 <samin.shafkat@gmail.com>
2021-04-11 23:13:52 +0600

    HeaderMain.js is styled

--- src/components/Home/HeaderMain/HeaderMain.js
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import chairImg from '../../../images/chair.png';
 
 const HeaderMain = () => {
     return (
-        <main className="row">
-            
-            
+        <main className="row h-100 align-items-center">
+            <div className="col-md-4 offset-md-1">
+                <h1 className="fw-bold">Your New Smile</h1>
+                <br />
+                <h1 className="fw-bold">Starts Here</h1>
+                <p className="text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the</p>
+                <button className="btn btn-primary">GET APPOINTMENT</button>
+            </div>
+            <div className="col-md-6">
+                <img src={chairImg} alt="" className="img-fluid" />
+            </div>
+
         </main>
     );
 };
--- src/components/Home/Navbar/Navbar.js
+++ src/components/Home/Navbar/Navbar.js
@@ -3,70 +3,35 @@ import React from 'react';
 const Navbar = () => {
     return (
 
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-md navbar-light">
+            <div className="container-fluid">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item active">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item active">
                             <a className="nav-link me-5" aria-current="page" href="#">Home</a>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item">
                             <a className="nav-link me-5" href="#">About</a>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item">
                             <a className="nav-link me-5" href="#">Dental Services</a>
                         </li>
-                        <li class="nav-item">
-                            <a className="nav-link me-5" href="#">Reviews</a>
+                        <li className="nav-item">
+                            <a className="nav-link me-5 text-light" href="#">Reviews</a>
                         </li>
-                        <li class="nav-item">
-                            <a className="nav-link me-5" href="#">Blog</a>
+                        <li className="nav-item">
+                            <a className="nav-link me-5 text-light" href="#">Blog</a>
                         </li>
-                        <li class="nav-item">
-                            <a className="nav-link me-5" href="#">Contact Us</a>
+                        <li className="nav-item">
+                            <a className="nav-link me-5 text-light" href="#">Contact Us</a>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
-
-
-
-
-        // <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        //     <div class="container-fluid">
-        //         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        //             <span class="navbar-toggler-icon"></span>
-        //         </button>
-
-        //         <div class="collapse navbar-collapse " id="navbarSupportedContent">
-        //             <ul class="navbar-nav ml-auto">
-        //                 <li class="nav-item active">
-        //                     <a className="nav-link" aria-current="page" href="#">Home</a>
-        //                 </li>
-        //                 <li class="nav-item">
-        //                     <a className="nav-link" href="#">About</a>
-        //                 </li>
-        //                 <li class="nav-item">
-        //                     <a className="nav-link" href="#">Dental Services</a>
-        //                 </li>
-        //                 <li class="nav-item">
-        //                     <a className="nav-link" href="#">Reviews</a>
-        //                 </li>
-        //                 <li class="nav-item">
-        //                     <a className="nav-link" href="#">Blog</a>
-        //                 </li>
-        //                 <li class="nav-item">
-        //                     <a className="nav-link" href="#">Contact Us</a>
-        //                 </li>
-        //             </ul>
-        //         </div>
-        //     </div>
-        // </nav>
-
     );
 };
 
